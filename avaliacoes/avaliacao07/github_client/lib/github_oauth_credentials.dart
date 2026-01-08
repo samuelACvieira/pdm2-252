8025bf46566d353fde127d39b88afd27c23c94f9
samuelACvieira <samuel.vieira63@aluno.ifce.edu.br>
2026-01-08 13:59:15 -0300

    atividade 07

---
+++ avaliacoes/avaliacao07/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,7 @@
+
+const githubClientId = 'Ov23liHhp9EPWHtqgKZr';
+const githubClientSecret = '81aa236258162327c51027c30e8c1be2a16593e1';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
+
